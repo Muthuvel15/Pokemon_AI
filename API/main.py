694f6f1ca315a97dbc8a408e5da5562df539a135
Damien SALEM <d.salem@ecole-ipssi.net>
2022-12-02 16:46:03 +0100

    bug sur l'api à résoudre

--- API/main.py
+++ API/main.py
@@ -4,6 +4,12 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 import shutil
+import keras
+import keras.models
+import numpy as np
+import matplotlib.pyplot as plt
+import io
+from PIL import Image
 
 app = FastAPI()
 
@@ -11,6 +17,8 @@ templates = Jinja2Templates(directory="templates")
 #app.mount("/static", StaticFiles(directory="static"), name="static")
 #app.mount("/json", StaticFiles(directory="json"), name="json")
 
+model_file_name='..\IA\models\\type_fire-water_finder.h5'
+model = keras.models.load_model(model_file_name)
 # recuper la page index dans le dossier templates
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
@@ -19,9 +27,19 @@ async def read_item(request: Request):
 #recuper variable mis dans le formulaire : 
 @app.post("/submitform")
 async def handle_form(user_image: UploadFile = File(...)):
-    with open("test", "wb") as buffer:
+    with open("buffer", "wb") as buffer:
         shutil.copyfileobj(user_image.file, buffer)
-    return {"filename" : user_image.filename, "Content_type": user_image.content_type, "file":user_image.file}
+        img=Image.open(user_image.file)
+        img.save(user_image.filename)
+        img.show(user_image.filename)
+        try:
+            class_probabilities = model.predict(img) # ERROR here
+            predictions = np.argmax(class_probabilities, axis=1)
+            print(predictions)
+        except IndexError as e:
+            # return {"error":e}
+            return {"filename" : user_image.filename, "Content_type": user_image.content_type, "file":user_image.file}
+    # return {"predi": predictions}
   
 
 
